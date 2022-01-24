d1c663e632adcf644f3b45538f276c2c65b156a3
KimDongGyun1 <potter1185@gmail.com>
2022-01-24 14:53:54 +0900

    Add files via upload

---
+++ 2장/js/text-color.js
@@ -0,0 +1,6 @@
+ // heading 변수를 선언하고 h1태그를 저장한 것이 됨
+ var heading = document.querySelector("#heading");
+ // h1태그를 클릭을 하면 글자색깔을 red로 설정한것
+ heading.onclick = function() {
+     heading.style.color = "red";
+ }
